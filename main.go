4f7c2502037c614b1c58dde290b6536e85097d9c
Karl Matthias <karl.matthias@gonitro.com>
2017-05-30 15:23:01 +0100

    Use a defined clustername

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/Nitro/filecache"
+	"github.com/Nitro/memberlist"
 	"github.com/Nitro/ringman"
 	log "github.com/Sirupsen/logrus"
 	"github.com/kelseyhightower/envconfig"
@@ -22,6 +23,7 @@ type Config struct {
 	ClusterSeeds []string `envconfig:"CLUSTER_SEEDS" default:"127.0.0.1"`
 	CacheSize    int      `envconfig:"CACHE_SIZE" default:"512"`
 	RedisPort    int      `envconfig:"REDIS_PORT" default:"6379"`
+	ClusterName  string   `envconfig""CLUSTER_NAME" default:"default"`
 }
 
 func main() {
@@ -32,7 +34,10 @@ func main() {
 	envconfig.Process("raster", &config)
 	rubberneck.NewPrinter(log.Infof, rubberneck.NoAddLineFeed).Print(config)
 
-	ring, err := ringman.NewDefaultMemberlistRing(config.ClusterSeeds, config.Port)
+	ring, err := ringman.NewMemberlistRing(
+		memberlist.DefaultLANConfig(),
+		config.ClusterSeeds, config.Port, config.ClusterName,
+	)
 	if err != nil {
 		log.Fatalf("Unble to establish memberlist ring: %s", err)
 	}
